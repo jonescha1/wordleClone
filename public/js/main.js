0e83f123ef856e5a320adb3be5e88a1844583864
Chase Jones <chasejones@Chases-MacBook-Pro.local>
2022-06-07 22:26:45 -0400

    Reworked code by breaking into functions

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,19 @@ document.addEventListener("DOMContentLoaded", () => {
   // Calls the function to create the game board
   buildBoard();
 
+  //Holds an array of words the user has guessed
+  let guessedWords = [[]];
+  let word = "chase"; //This is just to for testing against a correct word. Will pull in random words later on.
+  let availableSpace = 0;
+  let availableSpaceEl = document.getElementById(
+    `block-front-` + String(availableSpace)
+  );
+
+  // This will add the functionality for the user to input the character from the users keyboard
+  document.addEventListener("keydown", (e) => {
+    updateGuessedWords(e.key);
+  });
+
   // Builds the game board wireframe
   function buildBoard() {
     const board = document.getElementById("board");
@@ -18,14 +31,10 @@ document.addEventListener("DOMContentLoaded", () => {
       let back = document.createElement("div");
 
       front.setAttribute("class", "block-front");
+      front.setAttribute("id", `block-front-` + i);
       back.setAttribute("class", "block-back");
       back.setAttribute("id", `block-back-` + i);
 
-      let character = document.createElement("h2");
-      // innerhtml needs to be activated when user inputs a character.
-      character.innerHTML = i;
-
-      back.appendChild(character);
       inner.appendChild(front);
       inner.appendChild(back);
 
@@ -33,4 +42,75 @@ document.addEventListener("DOMContentLoaded", () => {
       board.appendChild(block);
     }
   }
+
+  //Gets the number of guessed words.
+  function getCurrentWordArr() {
+    const numberOfGuessedWords = guessedWords.length;
+    return guessedWords[numberOfGuessedWords - 1];
+  }
+
+  // Updates guessedWords array
+  function updateGuessedWords(letter) {
+    const currentWordArr = getCurrentWordArr();
+
+    if (
+      currentWordArr &&
+      currentWordArr.length < 5 &&
+      /^[a-zA-Z]$/gi.test(letter)
+    ) {
+      currentWordArr.push(letter);
+      availableSpaceEl = document.getElementById(
+        `block-front-` + String(availableSpace)
+      );
+
+      availableSpace = availableSpace + 1;
+      availableSpaceEl.textContent = letter;
+    } else if (letter === "Backspace" && availableSpace > 0) {
+      //This if statement is to prevent the user from being able to delete previous guesses. They will be limited to deletin only the characters in the current row/guess.
+      if (currentWordArr.length !== 0) {
+        currentWordArr.pop();
+        availableSpace = availableSpace - 1;
+        availableSpaceEl = document.getElementById(
+          `block-front-` + String(availableSpace)
+        );
+        availableSpaceEl.textContent = "";
+      }
+
+      // console.log(currentWordArr.length);
+    } else if (letter === "Enter") {
+      submitHandler();
+    }
+  }
+
+  // Handler for when the user hit the Enter/Return key
+  function submitHandler() {
+    // const board = document.querySelectorAll("#board");
+    const currentWordArr = getCurrentWordArr();
+    const currentWord = currentWordArr.join("");
+
+    if (currentWordArr.length == 5) {
+      if (currentWord == word) {
+        // user entered correct word
+        // Will need to clear the board and start over. Figure out what to do when user wins.
+        window.alert("Congrats!!!");
+        // ***********************This block is for testing purposes***********************
+        let blockParent = document.getElementById("1"); //******************************* */
+        let child = blockParent.querySelector(".block-inner"); //************************ */
+        child.classList.add("rotate"); //***************************************************
+        // ***********************This block is for testing purposes***********************
+      } else if (guessedWords.length === 6) {
+        //Will need to clear the board and start over. Figure out what to do in case the user exceeds the amounts of guesses.
+        window.alert("Sorry, no more guesses allowed");
+      }
+      //Add a new array to the guessedWords array for the next row.
+      guessedWords.push([]);
+    } else if (currentWordArr.length !== 5) {
+      //Need to add
+      window.alert("Word must be 5 letter");
+      console.log(guessedWords);
+    }
+  }
+
+  // ********************************
+  // Create a function that will flip the corresponding blocks after hitting enter.
 });
